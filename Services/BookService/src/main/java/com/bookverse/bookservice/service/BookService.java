9494cdd9b5869f5c6f791c10ed664348ce0a9ad7
Piyush Dugawa <rpiyushdugawa@gmail.com>
2026-07-24 18:10:41 +0000

    Add getAllBooks in Service class of BookService

--- Services/BookService/src/main/java/com/bookverse/bookservice/service/BookService.java
+++ Services/BookService/src/main/java/com/bookverse/bookservice/service/BookService.java
@@ -1,9 +1,27 @@
 package com.bookverse.bookservice.service;
 
+import com.bookverse.bookservice.DTO.BookResponseDTO;
+import com.bookverse.bookservice.mapper.BookMapper;
+import com.bookverse.bookservice.model.Book;
+import com.bookverse.bookservice.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public List<BookResponseDTO> findAllBooks(){
+        return bookRepository
+                .findAll()
+                .stream()
+                .map(bookMapper::toBookResponseDTO)
+                .toList()
+                ;
+    }
+
 }
