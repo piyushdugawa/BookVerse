ddbf58c5413f0d0b4084e522ff0d123f26538521
Piyush Dugawa <rpiyushdugawa@gmail.com>
2026-07-26 18:03:21 +0000

    Add Exception Handling to findBookById Service in BookService

--- Services/BookService/src/main/java/com/bookverse/bookservice/service/BookService.java
+++ Services/BookService/src/main/java/com/bookverse/bookservice/service/BookService.java
@@ -1,12 +1,15 @@
 package com.bookverse.bookservice.service;
 
 import com.bookverse.bookservice.DTO.BookResponseDTO;
+import com.bookverse.bookservice.exception.BookNotFoundException;
 import com.bookverse.bookservice.mapper.BookMapper;
 import com.bookverse.bookservice.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static java.lang.String.*;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +27,10 @@ public class BookService {
     }
 
     public  BookResponseDTO findBookById(int bookId){
-        return bookMapper.toBookResponseDTO(bookRepository.findById(bookId).orElse(null));
+        return bookMapper
+                .toBookResponseDTO(bookRepository
+                        .findById(bookId)
+                        .orElseThrow(() -> new BookNotFoundException("Book with id: {" +  bookId + "} not found!")));
     }
 
     //TODO: Add service methods for Rest calls from controller
