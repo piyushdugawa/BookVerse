7729ce06b20f8677cbc72b6d953030a5b382620d
Piyush Dugawa <rpiyushdugawa@gmail.com>
2026-08-02 13:05:26 +0530

    Change Postgres port to 5433 to resolve local postgres conflict and Add Default Timezone in Bookservice to avoid timezone issues

--- Services/BookService/src/main/java/com/bookverse/bookservice/BookServiceApplication.java
+++ Services/BookService/src/main/java/com/bookverse/bookservice/BookServiceApplication.java
@@ -3,10 +3,13 @@ package com.bookverse.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BookServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(BookServiceApplication.class, args);
     }
 
