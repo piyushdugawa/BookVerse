fac0d84b75f9f9383b844f96f76488b94e1cb370
Piyush Dugawa <rpiyushdugawa@gmail.com>
2026-08-03 19:45:38 +0530

    Replace FormLogin with HttpBasic and Disable CSRF

--- Services/AuthenticationService/src/main/java/com/bookverse/authenticationservice/config/SecurityConfig.java
+++ Services/AuthenticationService/src/main/java/com/bookverse/authenticationservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,7 +21,8 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .logout(Customizer.withDefaults())
                 .build();
     }
