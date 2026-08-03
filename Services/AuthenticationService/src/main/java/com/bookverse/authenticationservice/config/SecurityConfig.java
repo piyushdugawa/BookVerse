4b99b8b7567beabecbfbfec05590f579c65e1ffe
Piyush Dugawa <rpiyushdugawa@gmail.com>
2026-08-03 16:42:41 +0530

    Add SpringSecurityConfig with defaults

---
+++ Services/AuthenticationService/src/main/java/com/bookverse/authenticationservice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.bookverse.authenticationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
+                .build();
+    }
+
+}
